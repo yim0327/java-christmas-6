3396fce7e4ec08b3730eeda521c064bba1d6fac5
yim0327 <yim0327@naver.com>
2025-12-26 15:27:04 +0900

    feat(view): 주문 메뉴 및 개수 입력 구현
    - readMenuTypesAndNumbers() 메서드를 통해 주문할 메뉴와 개수를 입력받음
    - 올바른 입력 형식을 위한 안내 메시지를 출력함 (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -11,6 +11,11 @@ public class InputView {
         return Integer.parseInt(input);
     }
 
+    public String readMenuTypesAndNumbers() {
+        System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
+        return Console.readLine();
+    }
+
     private void validateDateBoundary(String input) {
         int parsedInput = Integer.parseInt(input);
 
